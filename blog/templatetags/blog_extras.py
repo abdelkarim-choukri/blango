605de402f00247d953223fa934b22325e33da91f
abdelkarim-choukri <choukriabde98@gmail.com>
2023-01-13 11:26:10 +0100

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,11 +3,12 @@ from django.utils.safestring import mark_safe
 from django.utils.html import escape
 from blog.models import Post
 from django.contrib.auth import get_user_model
+import logging
 from django import template
 user_model = get_user_model()
 register = template.Library()
 
-
+logger = logging.getLogger(__name__)
 @register.filter
 def author_details(author, current_user):
     if not isinstance(author, user_model):
@@ -72,4 +73,5 @@ def author_details_tag(context):
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
     return {"title": "Recent Posts", "posts": posts}
